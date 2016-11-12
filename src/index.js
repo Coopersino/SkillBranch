6fe542776aa95ea04a8e1e861a8c683e9adf4b4e
Vitaliy Sheshin <vitaliy_sh@MacBook-Pro-Vitaliy.local>
2016-11-13 01:08:13 +0300

    task2b solution

--- src/index.js
+++ src/index.js
@@ -3,12 +3,38 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
-app.get('/', (req, res) => {
-  res.json({
-    hello: 'JS World',
-  });
-});
+app.get('/taskB', (req, res) => {
+  const fullname = req.query.fullname.trim();
+  // Check if normal
+  if (!fullname) {
+    return res.send('Invalid fullname');
+  }
+  const re = new RegExp('[0-9_/$&+,:;=?@#|\'<>.^*()%!-]');
+  const match = fullname.search(re);
 
-app.listen(3000, () => {
-  console.log('Your app listening on port 3000!');
+  if (match !== -1) {
+    return res.send('Invalid fullname');
+  }
+  const arr = fullname.split(' ');
+  if (arr.length > 3) {
+    res.send('Invalid fullname');
+  } else {
+    let response = arr[arr.length - 1];
+    let patronymic = '';
+    let name = '';
+    if (arr.length - 2 >= 0) {
+      patronymic = arr[arr.length - 2];
+    }
+    if (arr.length - 3 >= 0) {
+      name = arr[arr.length - 3];
+    }
+    if (name) {
+      response = response + ' ' + name[0] + '.';
+    }
+    if (patronymic) {
+      response = response + ' ' + patronymic[0] + '.';
+    }
+    res.send(response);
+  }
 });
+app.listen(3000);
